5007efd5679a4f3b37d2ffe63f3689366dc9d4bf
GiannisKap <g.kapolosss@gmail.com>
2020-10-12 20:25:47 +0300

    Update b.py

--- b.py
+++ b.py
@@ -3,6 +3,6 @@ y= raw_input("onoma omadas B")
 z1= int(raw_input("set omadas A"))
 z2= int(raw_input("set omadas B"))
 if z1>z2:
-    print "winner is " + x+ " with set " +str(z1-z2)
+    print ("winner is " + x+ " with set " +str(z1-z2))
 else:
-    print "winner is " + y+ " with set " +str(z2-z1)
+    print ("winner is " + y+ " with set " +str(z2-z1))
